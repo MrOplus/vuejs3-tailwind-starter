766add78e25bffd418c244f5fe1418f7eb72c29a
Koorosh <mr.oplus@yahoo.fr>
2024-07-16 02:43:44 +0400

    Init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container-fluid bg-slate-950 text-white min-h-screen p-12">
+    <div class="mx-auto max-w-2xl text-center align-middle">
+      <h1 class="text-4xl font-bold">Vue 3 + Vite + TailwindCSS + Vue Router + TypeScript + Pinia</h1>
+      <p class="mt-4 text-lg">This is a Vue 3 project template with Vite, TailwindCSS, Vue Router, TypeScript, and Pinia.</p>
+      </div>
+  </div>
+</template>
